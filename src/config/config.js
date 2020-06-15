54e8f3abfc2e1319ff03f4cfdf4abe5dc200aa12
Lucas Rombaldi <lucas.rombaldi@focco.com.br>
2020-06-15 16:39:32 -0300

    CSS adjustments

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export const marvelApi = {
-    publicKey: '{publicKey}',
-    privateKey: '{privateKey}',
+    publicKey: 'ebfed38e4e08f777f181a6c414f521eb',
+    privateKey: '2c724f0c19fc3556825c4f7da0952c7839452cc8',
     baseUrl: 'https://gateway.marvel.com:443',
   }
   
