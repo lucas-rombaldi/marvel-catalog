613a3fc2314f2916063bd739c514c85e7b29bc5a
Lucas Rombaldi <lucas.rombaldi@gmail.com>
2020-06-18 10:49:56 -0300

    Commit user test

--- src/Components/character-list/character-list.test.js
+++ src/Components/character-list/character-list.test.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Provider } from "react-redux";
 import {
   render,
-  queryByLabelText,
-  queryByTestId,
   fireEvent,
   cleanup,
 } from "@testing-library/react";
