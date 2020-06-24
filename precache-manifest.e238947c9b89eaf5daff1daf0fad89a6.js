9f4725268ad13a5395a5014f7230502c04df5cf9
Lucas Rombaldi <lucas.rombaldi@gmail.com>
2020-06-24 20:46:24 -0300

    Updates

---
+++ precache-manifest.e238947c9b89eaf5daff1daf0fad89a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248cc892006f1f3bef9f87553afc1009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adb04caef68d4716f47f",
+    "url": "/static/css/main.c81acf2f.chunk.css"
+  },
+  {
+    "revision": "b5e3fbe74dadcf9e68f7",
+    "url": "/static/js/2.345be5be.chunk.js"
+  },
+  {
+    "revision": "e62c2e0b48460e61966074b230f00632",
+    "url": "/static/js/2.345be5be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb04caef68d4716f47f",
+    "url": "/static/js/main.eb512fac.chunk.js"
+  },
+  {
+    "revision": "b57c064990a4868e159e",
+    "url": "/static/js/runtime-main.b2e2cb7c.js"
+  },
+  {
+    "revision": "44b2fa2f262dab3254baf94993395ab7",
+    "url": "/static/media/marvel-logo.44b2fa2f.png"
+  },
+  {
+    "revision": "972da3497416d5b958b5717b221d687a",
+    "url": "/static/media/thanos.972da349.png"
+  }
+]);
