e7912cd00b422d18a3635ec183f9861667fb5a6b
Lucas Rombaldi <lucas.rombaldi@gmail.com>
2020-06-24 20:52:51 -0300

    Updates

---
+++ precache-manifest.03ca8d193a3565fd8b03166758102e62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b872d14db21a2cb903afa1e95dc73bd9",
+    "url": "/characters/index.html"
+  },
+  {
+    "revision": "8f233257de75c22837ee",
+    "url": "/characters/static/css/main.c81acf2f.chunk.css"
+  },
+  {
+    "revision": "c778ab7cd449b6fe99f4",
+    "url": "/characters/static/js/2.ec0e5b36.chunk.js"
+  },
+  {
+    "revision": "e62c2e0b48460e61966074b230f00632",
+    "url": "/characters/static/js/2.ec0e5b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f233257de75c22837ee",
+    "url": "/characters/static/js/main.7ca207df.chunk.js"
+  },
+  {
+    "revision": "5f1ad20759de03c0f0b7",
+    "url": "/characters/static/js/runtime-main.08001e78.js"
+  },
+  {
+    "revision": "44b2fa2f262dab3254baf94993395ab7",
+    "url": "/characters/static/media/marvel-logo.44b2fa2f.png"
+  },
+  {
+    "revision": "972da3497416d5b958b5717b221d687a",
+    "url": "/characters/static/media/thanos.972da349.png"
+  }
+]);
