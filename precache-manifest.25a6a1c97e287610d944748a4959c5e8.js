a991c8bdffb17510f5ab9645ceb3dfab715db899
lucas-rombaldi <57116102+lucas-rombaldi@users.noreply.github.com>
2020-06-30 23:11:32 +0000

    Deploying to gh-pages from  @ 55941b248a65937935dcf1dcb4b46608f06d7722 🚀

---
+++ precache-manifest.25a6a1c97e287610d944748a4959c5e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0db2c4bff692fd95fd57dd1318b831c",
+    "url": "/marvel-catalog/index.html"
+  },
+  {
+    "revision": "4f52a0cce14e5036e8bf",
+    "url": "/marvel-catalog/static/css/main.bbd76691.chunk.css"
+  },
+  {
+    "revision": "48aff290195707ed0c5b",
+    "url": "/marvel-catalog/static/js/2.b6f80fc7.chunk.js"
+  },
+  {
+    "revision": "e62c2e0b48460e61966074b230f00632",
+    "url": "/marvel-catalog/static/js/2.b6f80fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f52a0cce14e5036e8bf",
+    "url": "/marvel-catalog/static/js/main.8db7d188.chunk.js"
+  },
+  {
+    "revision": "44e2bf46c76b3333404a",
+    "url": "/marvel-catalog/static/js/runtime-main.77b01fb2.js"
+  },
+  {
+    "revision": "44b2fa2f262dab3254baf94993395ab7",
+    "url": "/marvel-catalog/static/media/marvel-logo.44b2fa2f.png"
+  },
+  {
+    "revision": "972da3497416d5b958b5717b221d687a",
+    "url": "/marvel-catalog/static/media/thanos.972da349.png"
+  }
+]);
